85c278a2dbd73ba788c7b76ee7dbbd610dcf2448
suryagangula <suryatejareddygangula@gmail.com>
2018-05-10 12:23:04 -0400

    #test checkin 2

--- app.js
+++ app.js
@@ -23,7 +23,7 @@
 // no matter where we actually lift from.
 // > Note: This is not required in order to lift, but it is a convenient default.
 process.chdir(__dirname);
-
+//feature checkin 1
 // Attempt to import `sails`.
 var sails;
 try {
